fcfc41ea3240d5b315ea77713c0e00984db0de39
dasari_sreenivasulu <134498474+dasarisreenivas@users.noreply.github.com>
2023-09-18 22:24:48 +0530

    Queue_implementation_using_arrays.java

--- javaprograms/collections_in_java/QUEUE/Queue_implementation_using_arrays.java
+++ javaprograms/collections_in_java/QUEUE/Queue_implementation_using_arrays.java
@@ -1,48 +1,66 @@
 package collections_in_java.QUEUE;
 import java.util.*;
 public class Queue_implementation_using_arrays {
-    public static void main(String[] args) {
+    public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        queue q = new queue(sc.nextInt());
-        for(int i=0;i<3;i++){
-            q.offer(sc.nextInt());
+        int n = sc.nextInt();
+        Queue s = new Queue(n);
+        for(int i=0;i<n;i++){
+            s.push(sc.nextInt());
         }
-        q.pop();
-        q.printqueue();
-
+        s.printStack();
+        System.out.println(s.peek());
+        System.out.println(s.pop());
+        System.out.println(s.isempty());
+        System.out.println(s.isfull());
+        System.out.println();
+        s.printStack();
+        sc.close();
     }
 }
-class queue{
+class Queue{
     private int maxSize;
-    private int top;
-    private int[]ArrayQueue;
-    public queue(int size){
-        maxSize=size;
-        ArrayQueue=new int [maxSize];
-        top=-1;
+    private int front;
+    private int rear;
+    private int arr[];
+    public Queue(int size){
+        this.maxSize= size;
+        this.front=0;
+        this.rear=-1;
+        this.arr=new int[maxSize];
     }
-    public void offer(int val){
-        if(top<maxSize-1) {
-            ArrayQueue[++top]=val;
-        }
-        else {
-            System.out.println("Queue ran out of memory");
-            return ;
+    public void push(int val){
+        if(isfull()){
+            System.out.println("queue is full");
+        }else{
+            arr[++rear]=val;
         }
     }
-    public void pop(){
-       if(top<0){
-        while(top!=0){
-        System.out.println("quqe elements are"+ArrayQueue[top--]);
-        top++;
+    public int peek(){
+        if(isempty()){
+            System.out.println("queue is empty we cannot peek the elements");
         }
-       }else{
-        System.out.println("Queue is empty");
-       }
+        return arr[front];
     }
-    public void printqueue(){
-        for(int i=0;i<ArrayQueue.length;i++){
-            System.out.println(ArrayQueue[i]);
+    public int pop(){
+        if(isempty()){
+            System.out.println("queue is full we cannot pop the elements");
+        }
+        return arr[front++];
+    }
+    public boolean isfull(){
+        if(rear== maxSize-1) return true;
+        return false;
+    }
+    public boolean isempty(){
+        if(front>rear){
+            return true;
+        }
+        return false;
+    }
+    public void printStack(){
+        for(int i =0;i<arr.length-1;i++){
+            System.out.println(arr[i]);
         }
     }
 }
