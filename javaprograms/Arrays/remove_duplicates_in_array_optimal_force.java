e73c54422f8373c1d3e77f856180a99e32874266
dasari_sreenivasulu <134498474+dasarisreenivas@users.noreply.github.com>
2023-09-26 22:34:17 +0530

    Add files via upload

---
+++ javaprograms/Arrays/remove_duplicates_in_array_optimal_force.java
@@ -0,0 +1,24 @@
+package Arrays;
+import java.util.*;
+public class remove_duplicates_in_array_optimal_force {
+    public static void main(String[]args){
+        try(Scanner sc =new Scanner(System.in)){
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        removing_elements(arr,n);
+    }
+}
+    public static void removing_elements(int []arr,int n){
+        int i =0;;
+        for(int j=1;j<arr.length;j++){
+            if(arr[j]!=arr[i]){
+                arr[i+1]= arr[j];
+                i++;
+            }
+        }
+        System.out.println(i+1);
+    }
+} 
