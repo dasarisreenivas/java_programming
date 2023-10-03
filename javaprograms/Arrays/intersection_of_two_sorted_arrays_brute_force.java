6edbf4aea141d7b8b8d7efbf64c0407547d5f43c
dasari_sreenivasulu <134498474+dasarisreenivas@users.noreply.github.com>
2023-10-03 21:12:47 +0530

    Add files via upload

---
+++ javaprograms/Arrays/intersection_of_two_sorted_arrays_brute_force.java
@@ -0,0 +1,27 @@
+package Arrays;
+import java.util.*;
+public class intersection_of_two_sorted_arrays_brute_force {
+    public static void main(String[]args){
+        ArrayList<Integer> intersection = new ArrayList<Integer>();
+        int arr1[]={1,2,3,3,4,5,6,7};
+        int arr2[]={2,3,3,4,5,6};
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        int visited[] = new int[n2];
+        for(int i =0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                if(arr1[i] == arr2[j] && visited[j]==0){
+                    intersection.add(arr1[i]);
+                    visited[j]=1;
+                    break;
+                }
+                if(arr2[j]>arr1[i]) break;
+            }
+        }
+        System.out.println(intersection);
+    }
+}
+
+/* time complexity = O(n1*n2)
+ * space complexity = O(n2)
+ */
